48707288e89487a4558cfa3bd07bd4510a81cb76
aisoulea <aisou202@gmail.com>
2023-02-23 16:07:22 +0900

    first commit

---
+++ simple_vpc/simple_vpc_stack.py
@@ -0,0 +1,40 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_ec2 as ec2,
+)
+from constructs import Construct
+
+class SimpleVpcStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # VPC
+        vpc = ec2.Vpc(
+            self,
+            "vpc",
+            max_azs=2,
+            nat_gateways=0,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="public", subnet_type=ec2.SubnetType.PUBLIC
+                ),
+                ec2.SubnetConfiguration(
+                    name="private", subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
+                ),
+            ],
+        )
+
+        vpc.add_interface_endpoint("ssm_endpoint",
+            service=ec2.InterfaceVpcEndpointAwsService.SSM
+        )
+
+        vpc.add_interface_endpoint("ec2messages_endpoint",
+            service=ec2.InterfaceVpcEndpointAwsService.EC2_MESSAGES
+        )
+
+        vpc.add_interface_endpoint("ssmmessages_endpoint",
+            service=ec2.InterfaceVpcEndpointAwsService.SSM_MESSAGES
+        )
+        
